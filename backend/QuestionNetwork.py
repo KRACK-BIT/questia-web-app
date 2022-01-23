a7d584bbbec2f303a539912dcee43c6af4ff4a69
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-01-23 11:08:06 +0000

    Modify API

--- backend/QuestionNetwork.py
+++ backend/QuestionNetwork.py
@@ -1,70 +1,88 @@
 from KeywordMatch import KeywordMatch
 from Tree import Tree
-from Topic import Topic 
-from Question import Question 
-class QuestionNetwork(): 
-  def __init__(self, head, children):
-    self.keyword_matcher = KeywordMatch()
-    self.head = self.text_to_topic_tree_object(head, 0)
-    self.head.add_child_list([self.text_to_topic_tree_object(child, 1) for child in children])
-    self.all_questions = {child.value.id : child.value for child in self.head.children}
+from Topic import Topic
+from Question import Question
 
-  def text_to_topic_tree_object(self, text: str, level: int) -> Tree:
-    topic = Topic(text)
-    tree = Tree(topic, level)
-    topic.add_parent(tree)
-    return tree 
 
-  def text_to_question_tree_object(self, text: str, level: int) -> Tree:
-    topic = Question(text)
-    tree = Tree(topic, level)
-    topic.add_parent(tree)
-    return tree
+class QuestionNetwork:
+    def __init__(self, head, children):
+        self.current_id = 5
+        self.keyword_matcher = KeywordMatch()
+        self.head = self.text_to_topic_tree_object(head, 0, False)
+        self.head.add_child_list(
+            [self.text_to_topic_tree_object(child, 1, True) for child in children]
+        )
+        self.all_questions = {
+            child.value.id: child.value for child in self.head.children
+        }
+        self.all_questions[self.head.value.id] = self.head
 
-  def network_to_JSON_format(self):
+    def text_to_topic_tree_object(self, text: str, level: int, is_head) -> Tree:
+        topic = Topic(self.current_id, text)
+        self.current_id += 1
+        tree = Tree(topic, level, self.head if is_head else None)
+        topic.add_parent(tree)
+        return tree
 
-    def recursive_search(node, connections): 
-      for child in node.children: 
-        connections.append({"source":node.value.id, "target":child.value.id})
+    def text_to_question_tree_object(self, text: str, level: int) -> Tree:
+        topic = Question(self.current_id, text)
+        self.current_id += 1
+        tree = Tree(topic, level)
+        topic.add_parent(tree)
+        return tree
 
-    connections = []
-    recursive_search(self.head, connections)
+    def network_to_JSON_format(self):
+        def recursive_search(node, connections):
+            for child in node.children:
+                connections.append({"source": node.value.id, "target": child.value.id})
 
-    return {
-      "links" : connections,
-      "nodes" : [node.parent.to_JSON_format() for node in self.all_questions.values()] 
-    }
+        connections = []
+        recursive_search(self.head, connections)
 
-  def get_potential_link(self, text): 
-    
-    def search_for_link(node):
-      if not node:
-        return False
+        return {
+            "links": connections,
+            "nodes": [
+                *[
+                    node.parent.to_JSON_format()
+                    for node in self.all_questions.values()
+                    if node.parent
+                ],
+            ],
+        }
 
-      value = node.value
-      if node.type == "Question" and self.keyword_matcher.find_keyword_match(text, value.text): 
-        return value.id
-      else:
-        for child in node.children:
-          search_result = search_for_link(child)
-          if search_result:
-            return search_result
-        return False 
+    def get_potential_link(self, text):
+        def search_for_link(node):
+            if not node:
+                return False
 
-    return search_for_link(self.head)
-  
-  def upvote_question(self, question_id):
-    self.all_questions[question_id] += 1
+            value = node.value
+            if node.type == "Question" and self.keyword_matcher.find_keyword_match(
+                text, value.text
+            ):
+                return value.id
+            else:
+                for child in node.children:
+                    search_result = search_for_link(child)
+                    if search_result:
+                        return search_result
+                return False
 
-  def add_question(self, text, link_id): 
-    link_val = self.all_questions[link_id]
-    link_node = link_val.parent
-    link_node.add_child(self.text_to_question_tree_object(text, link_node.level + 1))
+        return search_for_link(self.head)
 
-  def pprint(self):
-    def pprint_recurse(node):
-      node.pprint()
-      for child in node.children: 
-        pprint_recurse(child)
+    def upvote_question(self, question_id):
+        self.all_questions[question_id] += 1
 
-    pprint_recurse(self.head)
+    def add_question(self, text, link_id):
+        link_val = self.all_questions[link_id]
+        link_node = link_val.parent
+        link_node.add_child(
+            self.text_to_question_tree_object(text, link_node.level + 1)
+        )
+
+    def pprint(self):
+        def pprint_recurse(node):
+            node.pprint()
+            for child in node.children:
+                pprint_recurse(child)
+
+        pprint_recurse(self.head)
