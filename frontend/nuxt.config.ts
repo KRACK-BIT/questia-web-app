2f21833a47f68fdeded6d1aef33bafd35ff6f064
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-02-08 00:13:53 +0000

    Add presentation base

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -43,5 +43,6 @@ export default defineNuxtConfig({
 
   bridge: {
     vite: true,
+    meta: true,
   },
 })
---
+++ frontend/pages/presentation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="fill-screen">
+    <div class="reveal">
+      <div class="slides">
+        <section>Slide 1</section>
+        <section>Slide 2</section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Reveal from 'reveal.js'
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
+
+onMounted(() => {
+  let deck = new Reveal({
+    plugins: [Markdown],
+  })
+  deck.initialize()
+})
+
+useMeta({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: '/node_modules/reveal.js/dist/reveal.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: '/node_modules/reveal.js/dist/theme/black.css',
+    },
+  ],
+})
+</script>
+
+<style>
+.fill-screen {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
